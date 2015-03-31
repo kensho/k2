cb2e47875e373b5c4583fa85110292372e656177
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-03-31 11:34:43 -0400

    splitting find matches from ranking

---
+++ src/find-partial-matches-spec.js
@@ -0,0 +1,81 @@
+/* global describe, beforeEach, it */
+require('lazy-ass');
+var check = require('check-more-types');
+var _ = require('lodash');
+
+describe('partial matching', function () {
+  var matching = require('./find-partial-matches');
+  var findPartialMatches;
+
+  beforeEach('has methods for matching', function () {
+    la(check.object(matching));
+    la(check.fn(matching.findPartialMatches));
+    findPartialMatches = matching.findPartialMatches;
+  });
+
+  describe('findPartialMatches', function () {
+    it('returns empty list for empty query', function () {
+      la(check.fn(findPartialMatches));
+      var result = findPartialMatches('foo', [], '');
+      la(_.isEqual(result, []));
+    });
+
+    it('selects partial matches', function () {
+      var items = [{
+        foo: 'foo'
+      }, {
+        foo: 'foo2'
+      }, {
+        foo: 'bar'
+      }];
+      var result = findPartialMatches('foo', items, 'ar');
+      la(result.length === 1);
+      la(check.same(result[0], items[2]));
+    });
+
+    it('selects partial matches', function () {
+      var items = [{
+        foo: 'foo'
+      }, {
+        foo: 'foo2'
+      }, {
+        foo: 'bar'
+      }];
+      var result = findPartialMatches('foo', items, 'oo');
+      la(result.length === 2);
+      la(check.same(result[0], items[0]));
+      la(check.same(result[1], items[1]));
+    });
+  });
+
+  describe('findPartialMatches over multiple properties', function () {
+    it('selects partial matches', function () {
+      var items = [{
+        foo: 'foo'
+      }, {
+        foo: 'foo2',
+        bar: 'bar'
+      }, {
+        foo: 'bar'
+      }];
+      var result = findPartialMatches(['foo', 'bar'], items, 'ar');
+      la(result.length === 2);
+      la(check.same(result[0], items[1]));
+      la(check.same(result[1], items[2]));
+    });
+
+    it('property might be missing', function () {
+      var items = [{
+        foo: 'foo'
+      }, {
+        foo: 'foo2',
+        bar: 'bar'
+      }, {
+        foo: 'bar'
+      }];
+      var result = findPartialMatches(['nothing', 'bar'], items, 'ar');
+      la(result.length === 1);
+      la(check.same(result[0], items[1]));
+    });
+  });
+});
---
+++ src/find-partial-matches.es6
@@ -0,0 +1,46 @@
+require('lazy-ass');
+var check = require('check-more-types');
+var _ = require('lodash');
+
+function findPartialMatchesSingleProperty(property, items, queryText) {
+  la(check.unemptyString(property), 'need property name', property);
+  la(check.array(items), 'expected list of items', items);
+  la(check.string(queryText), 'expected query string', queryText);
+  if (!queryText) {
+    return [];
+  }
+
+  var text = queryText.toLowerCase();
+  function hasQueryText(item) {
+    return item[property].toLowerCase().indexOf(text) !== -1;
+  }
+
+  return items.filter(hasQueryText);
+}
+
+function findPartialMatchesMultipleProperties(properties, items, queryText) {
+  if (check.string(properties)) {
+    return findPartialMatchesSingleProperty(properties, items, queryText);
+  }
+  la(check.arrayOfStrings(properties), 'need properties', properties);
+  la(check.array(items), 'expected list of items', items);
+  la(check.string(queryText), 'expected query string', queryText);
+  if (!queryText) {
+    return [];
+  }
+
+  var text = queryText.toLowerCase();
+  function hasQueryText(item) {
+    return properties.some(function (property) {
+      var value = item[property];
+      if (!value) {
+        return false;
+      }
+      return value.toLowerCase().indexOf(text) !== -1;
+    });
+  }
+
+  return items.filter(hasQueryText);
+}
+
+export findPartialMatches findPartialMatchesMultipleProperties;
